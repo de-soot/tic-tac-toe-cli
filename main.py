3b9289bac67f69236a6600c9307577dd48ff978a
de_soot <78423238+de-soot@users.noreply.github.com>
2023-12-28 06:26:44 +0000

    Update main.py
    Signed-off-by: de_soot <78423238+de-soot@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,67 +1,74 @@
-board = [
-        ['1', '2', '3'],
-        ['4', '5', '6'],
-        ['7', '8', '9']
-        ]
- 
- 
-def display_board():
+def display_board(board_):
     for i in range(3):
-        print("  " + board[i][0] + "  │  " + board[i][1] + "  │  " + board[i][2])
+        print("  " + board_[i][0] + "  │  " + board_[i][1] + "  │  " + board_[i][2])
         print("─────┼─────┼─────")
+    
+    print("\n")
  
  
-def X_O():
-    if input_position < 1 or input_position > 9:
-        print("Out of bounds, try again")
+def plot(board_, input_position_, X_or_O_):
+    if input_position_ < 1 or input_position_ > 9:
+        print("Out of bounds, try again\n")
         return 1
  
-    y = (input_position - 1) // 3
-    x = input_position % 3 - 1
+    y = (input_position_ - 1) // 3
+    x = (input_position_ % 3) - 1
  
-    if board[y][x] == 'X' or board[y][x] == 'O':
-        print("Already taken, try again")
+    if (board_[y][x] == 'X') or (board_[y][x] == 'O'):
+        print("Already taken, try again\n")
         return 1
  
-    board[y][x] = X_or_O
+    board_[y][x] = X_or_O_
  
     return 0
  
  
- 
-def win():
+def win(board_, X_or_O_):
     # 8 total checks for 3 horizontal, 3 vertical, and 2 diagonal rows
-    if (board[0][0] == X_or_O and board[0][1] == X_or_O and board[0][2] == X_or_O) or\
-       (board[1][0] == X_or_O and board[1][1] == X_or_O and board[1][2] == X_or_O) or\
-       (board[2][0] == X_or_O and board[2][1] == X_or_O and board[2][2] == X_or_O) or\
-       (board[0][0] == X_or_O and board[1][0] == X_or_O and board[2][0] == X_or_O) or\
-       (board[0][1] == X_or_O and board[1][1] == X_or_O and board[2][1] == X_or_O) or\
-       (board[0][2] == X_or_O and board[1][2] == X_or_O and board[2][2] == X_or_O) or\
-       (board[0][0] == X_or_O and board[1][1] == X_or_O and board[2][2] == X_or_O) or\
-       (board[2][0] == X_or_O and board[1][1] == X_or_O and board[0][2] == X_or_O):
-        return True
- 
-    return False
- 
- 
-user_input = input("Press enter to play a game of tic-tac-toe / noughts-and-crosses or type 'quit' to exit the program : ").lower()
- 
-while user_input != "quit":
-    input_position = 0
+    return (board_[0][0] == X_or_O_ and board_[0][1] == X_or_O_ and board_[0][2] == X_or_O_) or\
+           (board_[1][0] == X_or_O_ and board_[1][1] == X_or_O_ and board_[1][2] == X_or_O_) or\
+           (board_[2][0] == X_or_O_ and board_[2][1] == X_or_O_ and board_[2][2] == X_or_O_) or\
+           (board_[0][0] == X_or_O_ and board_[1][0] == X_or_O_ and board_[2][0] == X_or_O_) or\
+           (board_[0][1] == X_or_O_ and board_[1][1] == X_or_O_ and board_[2][1] == X_or_O_) or\
+           (board_[0][2] == X_or_O_ and board_[1][2] == X_or_O_ and board_[2][2] == X_or_O_) or\
+           (board_[0][0] == X_or_O_ and board_[1][1] == X_or_O_ and board_[2][2] == X_or_O_) or\
+           (board_[2][0] == X_or_O_ and board_[1][1] == X_or_O_ and board_[0][2] == X_or_O_)
+
+
+def main():
+    user_input = input("Press enter to play a game of tic-tac-toe / noughts-and-crosses or type 'quit' to exit the program : ").lower()
+
+    if user_input == "quit":
+        return
+
+    # initialise variables
+    board = [
+            ['1', '2', '3'],
+            ['4', '5', '6'],
+            ['7', '8', '9']
+            ]
+
     count = 0
     x_turn = False
-    X_or_O = ''
- 
-    while not win() and count < 9:
+    X_or_O = 'X'
+    
+    # game start
+    while not win(board, X_or_O) and (count < 9):
         count += 1
-        display_board()
+        display_board(board)
         x_turn = (count % 2 == 1)
-        X_or_O = 'X' * (x_turn) + 'O' * (not x_turn)
+        X_or_O = ( 'X' * (x_turn) ) + ( 'O' * (not x_turn) )
  
         print(X_or_O + "'s turn")
-        input_position = int(input("Position : "))
-        count -= X_O()
-   
-    display_board()
-    print((X_or_O + " is the winner !") * (win()) + ("It's a tie !") * (not win()))
-    user_input = input("Press enter to play another game of tic-tac-toe/noughts-and-crosses or type 'quit' to exit the program : ").lower()
+        input_position = int( input("Position : ") )
+        count -= plot(board, input_position, X_or_O)
+    # game end
+
+    # display results
+    display_board(board)
+    has_winner = win(board, X_or_O)
+    print( (X_or_O + " is the winner !\n") * ( has_winner ) + ("It's a tie !\n") * ( not has_winner ) )
+    main()
+
+if __name__ == "__main__":
+    main()
